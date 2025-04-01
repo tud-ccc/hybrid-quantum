280d9f1404463c7747c15823f1a3c5d2868d4450
Lars Schütze <lars.schuetze@tu-dresden.de>
2025-04-01 11:23:08 +0200

    Full CMAKE based build and test

---
+++ test/Integration/qir-integration-test.mlir
@@ -0,0 +1,39 @@
+// RUN: quantum-opt %s \
+// RUN:   --pass-pipeline="builtin.module( \
+// RUN:       convert-quantum-to-qir, \
+// RUN:       func.func(convert-scf-to-cf), \
+// RUN:       convert-qir-to-llvm, \
+// RUN:       convert-func-to-llvm, \
+// RUN:       convert-cf-to-llvm, \
+// RUN:       convert-vector-to-llvm, \
+// RUN:       one-shot-bufferize{allow-unknown-ops}, \
+// RUN:       finalize-memref-to-llvm, \
+// RUN:       convert-index-to-llvm, \
+// RUN:       convert-arith-to-llvm, \
+// RUN:       reconcile-unrealized-casts)" | \
+// RUN: mlir-cpu-runner -e entry -entry-point-result=void \
+// RUN:     --shared-libs=%qir_shlibs,%mlir_c_runner_utils | \
+// RUN: FileCheck %s --match-full-lines
+
+module {
+
+  // Function to allocate a qubit, apply an X gate, measure and read the result
+  func.func @test_0_X_returns_1() -> ()  {
+    %q = "quantum.alloc"() : () -> (!quantum.qubit<1>)
+    %q1 = "quantum.X" (%q) : (!quantum.qubit<1>) -> (!quantum.qubit<1>)
+    %mt, %q_m = "quantum.measure" (%q1) : (!quantum.qubit<1>) -> (tensor<1xi1>, !quantum.qubit<1>)
+    "quantum.deallocate"(%q_m) : (!quantum.qubit<1>) -> ()
+    %i = "index.constant" () {value = 0 : index} : () -> (index)
+    %m = "tensor.extract" (%mt, %i) : (tensor<1xi1>, index) -> (i1)
+    vector.print %m : i1
+    return
+  }
+
+  func.func @entry() {
+    // CHECK: 1
+    func.call @test_0_X_returns_1() : () -> ()
+    
+    return
+  }
+}
+
--- test/lit.cfg.py
+++ test/lit.cfg.py
@@ -28,6 +28,23 @@ config.test_source_root = os.path.dirname(__file__)
 # test_exec_root: The root path where tests should be run.
 config.test_exec_root = os.path.join(config.quantum_obj_root, 'test')
 
+# Searches for a runtime library with the given name and returns the found path.
+# Correctly handles the platforms shared library directory and naming conventions.
+def find_runtime(dir, name):
+    path = ""
+    for prefix in ["", "lib"]:
+        path = os.path.join(
+            dir, f"{prefix}{name}{config.llvm_shlib_ext}"
+        )
+        if os.path.isfile(path):
+            break
+    return path
+
+# Searches for a runtime library with the given name and returns a tool
+# substitution of the same name and the found path.
+def add_runtime(name):
+    return ToolSubst(f"%{name}", find_runtime(config.llvm_lib_dir, name))
+
 # excludes: A list of directories to exclude from the testsuite. The 'Inputs'
 # subdirectories contain auxiliary inputs for various tests in their parent
 # directories.
@@ -35,6 +52,9 @@ config.excludes = ['Inputs', 'CMakeLists.txt', 'README.md', 'LICENSE.txt']
 
 config.substitutions.append(('%PATH%', config.environment['PATH']))
 config.substitutions.append(('%shlibext', config.llvm_shlib_ext))
+config.substitutions.append(("%llvm_src_root", config.llvm_src_root))
+
+config.substitutions.append(('%qir_shlibs', config.qir_shlibs))
 
 llvm_config.with_system_environment(['HOME', 'INCLUDE', 'LIB', 'TMP', 'TEMP'])
 
@@ -46,6 +66,11 @@ llvm_config.with_environment('PATH', config.llvm_tools_dir, append_path=True)
 llvm_config.with_environment('PATH', config.quantum_tools_dir, append_path=True)
 
 tool_dirs = [config.quantum_tools_dir, config.llvm_tools_dir]
-tools = ['quantum-opt']
+tools = [
+    "quantum-opt",
+    "mlir-cpu-runner",
+    add_runtime("mlir_runner_utils"),
+    add_runtime("mlir_c_runner_utils"),
+]
 
 llvm_config.add_tool_substitutions(tools, tool_dirs)
