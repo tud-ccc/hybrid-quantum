536c124eb7b563d46af060a84bb43dd52f2e86b8
Lars Schütze <lars.schuetze@tu-dresden.de>
2025-06-11 13:22:34 +0200

    Extend hoisting to more ops

--- lib/Dialect/Quantum/Transforms/ControlFlowHoisting.cpp
+++ lib/Dialect/Quantum/Transforms/ControlFlowHoisting.cpp
@@ -9,11 +9,15 @@
 #include "mlir/Transforms/GreedyPatternRewriteDriver.h"
 #include "quantum-mlir/Dialect/Quantum/IR/Quantum.h"
 #include "quantum-mlir/Dialect/Quantum/IR/QuantumOps.h"
+#include "quantum-mlir/Dialect/Quantum/IR/QuantumTypes.h"
 
 #include <llvm/ADT/STLExtras.h>
+#include <llvm/ADT/SmallPtrSet.h>
 #include <llvm/Support/Casting.h>
+#include <mlir/IR/IRMapping.h>
 #include <mlir/IR/OperationSupport.h>
 #include <mlir/IR/Value.h>
+#include <mlir/IR/ValueRange.h>
 #include <mlir/Support/LLVM.h>
 
 using namespace mlir;
@@ -42,15 +46,29 @@ struct ControlFlowHoistingPass : mlir::quantum::impl::ControlFlowHoistingBase<
 struct HoistOperations : OpRewritePattern<IfOp> {
     using OpRewritePattern<IfOp>::OpRewritePattern;
 
-    LogicalResult
-    matchAndRewrite(IfOp op, PatternRewriter &rewriter) const override
+    void findOperandsToIfArgs(
+        IfOp op,
+        OperandRange operands,
+        IRMapping &mapping) const
+    {
+        for (auto operand : operands)
+            if (auto blockArg = dyn_cast<BlockArgument>(operand)) {
+                unsigned index = blockArg.getArgNumber();
+                // IfOp has condition + block args
+                mapping.map(operand, op->getOperand(index + 1));
+            } else
+                assert(false && "Not a block argument");
+    }
+
+    LogicalResult findEquivalentOperations(
+        SmallPtrSetImpl<Operation*> &markMove,
+        SmallPtrSetImpl<Operation*> &markDelete,
+        Region &region1,
+        Region &region2) const
     {
         bool hasApplied = false;
-        SmallPtrSet<Operation*, 4> markDelete;
-        SmallPtrSet<Operation*, 4> markMove;
-        for (auto [thenArg, elseArg] : llvm::zip(
-                 op.getThenRegion().getArguments(),
-                 op.getElseRegion().getArguments())) {
+        for (auto [thenArg, elseArg] :
+             llvm::zip(region1.getArguments(), region2.getArguments())) {
             DenseMap<Value, Value> valuesMap;
             auto mapValue = [&](Value lhs, Value rhs) {
                 if (!dyn_cast<BlockArgument>(lhs)
@@ -61,7 +79,8 @@ struct HoistOperations : OpRewritePattern<IfOp> {
             };
             for (auto [thenOp, elseOp] :
                  llvm::zip(thenArg.getUsers(), elseArg.getUsers())) {
-                if (OperationEquivalence::isEquivalentTo(
+                if (!llvm::isa<YieldOp>(thenOp) && !llvm::isa<YieldOp>(elseOp)
+                    && OperationEquivalence::isEquivalentTo(
                         thenOp,
                         elseOp,
                         mapValue,
@@ -73,28 +92,54 @@ struct HoistOperations : OpRewritePattern<IfOp> {
                 }
             }
         }
+        return success(hasApplied);
+    }
+
+    LogicalResult
+    matchAndRewrite(IfOp op, PatternRewriter &rewriter) const override
+    {
+        // Find equivalent operations in both branches whose operands
+        // depend on the branch's block arguments
+        SmallPtrSet<Operation*, 4> markDelete;
+        SmallPtrSet<Operation*, 4> markMove;
+        if (failed(findEquivalentOperations(
+                markMove,
+                markDelete,
+                op.getThenRegion(),
+                op.getElseRegion())))
+            return failure();
 
         for (auto moveOp : markMove) {
-            SmallVector<Value, 4> operands;
+            // Remap cloned operation operands to if operands
+            IRMapping mapping;
+            findOperandsToIfArgs(op, moveOp->getOperands(), mapping);
+
+            // Clone the operation in front of if
+            rewriter.setInsertionPoint(op);
+            auto clonedOp = rewriter.clone(*moveOp, mapping);
+
+            // Replace the operation from each branch by replacing its uses
+            SmallVector<Value, 4> replacements;
             for (auto operand : moveOp->getOperands())
-                if (auto blockArg = dyn_cast<BlockArgument>(operand)) {
-                    unsigned index = blockArg.getArgNumber();
-                    // IfOp has condition + block args
-                    operands.push_back(op->getOperand(index + 1));
-                } else
-                    assert(false && "Not a block argument");
+                if (llvm::isa<QubitType>(operand.getType()))
+                    replacements.push_back(operand);
+            rewriter.replaceOp(moveOp, replacements);
 
-            rewriter.replaceAllUsesWith(
-                moveOp->getResults(),
-                moveOp->getOperands());
-            rewriter.moveOpBefore(moveOp, op);
-            rewriter.replaceAllUsesWith(operands, moveOp->getResults());
-            moveOp->setOperands(operands);
+            // Remap if operands to cloned operation results
+            for (auto [arg, res] :
+                 llvm::zip(clonedOp->getOperands(), clonedOp->getResults()))
+                op->replaceUsesOfWith(arg, res);
         }
-        for (auto delOp : markDelete)
-            rewriter.replaceOp(delOp, delOp->getOperands());
+        // Replace the operation from each branch by replacing its uses
+        for (auto delOp : markDelete) {
+            SmallVector<Value, 4> replacements;
+            for (auto operand : delOp->getOperands())
+                if (llvm::isa<QubitType>(operand.getType()))
+                    replacements.push_back(operand);
+            rewriter.replaceOp(delOp, replacements);
+        }
 
-        return success(hasApplied);
+        return success();
     }
 };
 
